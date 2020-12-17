5bf2e9ca24451f95a920e0e12622c57c5f7b5fd3
Rahul Jain <rahulkj@gmail.com>
2020-12-16 22:17:56 -0600

    first commit

---
+++ src/main/java/com/rahul/SampleK8sAppApplication.java
@@ -0,0 +1,21 @@
+package com.rahul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SampleK8sAppApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleK8sAppApplication.class, args);
+	}
+
+}
