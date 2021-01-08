20d10a7a8f17d68ee4ed157eeecfa0afd171c141
Rahul Jain <rahulkj@gmail.com>
2021-01-07 20:04:26 -0600

    Sprint boot reactive code

--- src/main/java/com/rahul/handler/GreetingHandler.java
+++ src/main/java/com/rahul/handler/GreetingHandler.java
@@ -1,5 +1,6 @@
 package com.rahul.handler;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -10,9 +11,12 @@ import reactor.core.publisher.Mono;
 
 @Component
 public class GreetingHandler {
+	
+	@Value("${greetingMessage:Hello from k8s Sample App}")
+	String greetingMessage;
 
 	public Mono<ServerResponse> hello(ServerRequest request) {
-		return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(BodyInserters.fromValue("Hello, Spring!"));
+		return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(BodyInserters.fromValue(greetingMessage));
 	}
 
 }
